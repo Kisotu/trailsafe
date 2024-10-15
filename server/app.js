71e656f1cd51809121572210b0abef5c860a0b66
Oloishorua <jacoboloishorua@gmail.com>
2024-10-15 17:38:22 +0300

    imported pins router

--- server/app.js
+++ server/app.js
@@ -1,21 +1,19 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './db.js';
-import userRouter from './routes/User.js'
-
-
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db.js";
+import userRouter from "./routes/User.js";
+import pinRouter from "./routes/Pins.js";
 
 dotenv.config();
 
 // Initialize express app
 const app = express();
 
-
 //middlewares
 app.use(express.json()); // parses JSON data
 app.use(express.urlencoded({ extended: true })); // parses URL-encoded data
 
-
 //routes
-app.use('/users', userRouter);
+app.use("/users", userRouter);
+app.use("/pins", pinRouter);
 export default app;
