d3775bad216a00766428128e9498100a66cb3e73
Oloishorua <jacoboloishorua@gmail.com>
2024-10-16 21:28:45 +0300

    updated user schema to strengthen validations

--- server/models/User.js
+++ server/models/User.js
@@ -3,10 +3,23 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-	name: String,
-	email: String,
-	password: String
-})
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
 
 const User = mongoose.model('User', UserSchema);
 
