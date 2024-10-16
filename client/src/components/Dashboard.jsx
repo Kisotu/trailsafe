dc831c3f3cb0863ceda358bb412ee8ee17eb51d4
Oloishorua <jacoboloishorua@gmail.com>
2024-10-16 21:24:42 +0300

    created dashboard component

---
+++ client/src/components/Dashboard.jsx
@@ -0,0 +1,63 @@
+import { useUser } from '@clerk/clerk-react';
+import { BarChart, Users, FileText } from 'lucide-react';
+import MyMap from './Map';
+
+const Dashboard = () => {
+  const { user } = useUser();
+
+  return (
+    <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <h1 className="text-3xl font-bold text-gray-900 mb-6">Welcome to your Dashboard, {user?.firstName}!</h1>
+      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="bg-white overflow-hidden shadow rounded-lg">
+          <div className="p-5">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <BarChart className="h-6 w-6 text-gray-400" />
+              </div>
+              <div className="ml-5 w-0 flex-1">
+                <dl>
+                  <dt className="text-sm font-medium text-gray-500 truncate">Marked Trails</dt>
+                  <dd className="text-lg font-medium text-gray-900">240</dd>
+                </dl>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white overflow-hidden shadow rounded-lg">
+          <div className="p-5">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <Users className="h-6 w-6 text-gray-400" />
+              </div>
+              <div className="ml-5 w-0 flex-1">
+                <dl>
+                  <dt className="text-sm font-medium text-gray-500 truncate">Trail Users</dt>
+                  <dd className="text-lg font-medium text-gray-900">234</dd>
+                </dl>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white overflow-hidden shadow rounded-lg">
+          <div className="p-5">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <FileText className="h-6 w-6 text-gray-400" />
+              </div>
+              <div className="ml-5 w-0 flex-1">
+                <dl>
+                  <dt className="text-sm font-medium text-gray-500 truncate">Total Pins</dt>
+                  <dd className="text-lg font-medium text-gray-900">506</dd>
+                </dl>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <MyMap />
+    </div>
+  );
+};
+
+export default Dashboard;
