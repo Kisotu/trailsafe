d926530029bdf0a874f03b87a41fd575f4fca092
Oloishorua <jacoboloishorua@gmail.com>
2024-10-16 21:26:27 +0300

    added  middlewares to be used in the app

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db.js";
-import userRouter from "./routes/User.js";
+import authRouter from "./routes/Auth.js";
 import pinRouter from "./routes/Pins.js";
+import userRouter from './routes/Users.js'
+import cors from 'cors'
 
 dotenv.config();
 
@@ -12,8 +14,16 @@ const app = express();
 //middlewares
 app.use(express.json()); // parses JSON data
 app.use(express.urlencoded({ extended: true })); // parses URL-encoded data
+app.use(
+	cors({
+		credentials: true,
+		origin: 'http://localhost:5173'
+	})
+)
 
 //routes
-app.use("/users", userRouter);
+app.use("/auth", authRouter);
 app.use("/pins", pinRouter);
+app.use('/users', userRouter);
+
 export default app;
