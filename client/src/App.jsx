709e69b48e1f04b56dcc99f52c2a09ba78e605b8
Oloishorua <jacoboloishorua@gmail.com>
2024-10-16 21:24:25 +0300

    created routes for the app and implemented protected routes and actions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,45 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+import Pins from './components/Pins';
 
-function App() {
 
+const App = () => {
   return (
-    <>
-    <p>Hello</p>
-    </>
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/dashboard"
+          element={
+            <>
+              <SignedIn>
+                <Dashboard />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
+        <Route
+          path="/pins"
+          element={
+            <>
+              <SignedIn>
+                <Pins />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
+      </Routes>
+    </div>
   )
 }
-
-export default App
+export default App;
