4441a6ef722a07ea62bf8b15c0e7df6df5ae0748
Oloishorua <jacoboloishorua@gmail.com>
2024-10-16 21:29:40 +0300

    reformated the pins routes by removing the crud operations and movinf them to the controller file

--- server/routes/Pins.js
+++ server/routes/Pins.js
@@ -1,61 +1,21 @@
 import express from "express";
-
-//pin
-import { Pin } from "../models/Pin.js";
+import PinController from "../controllers/PinController.js";
 
 const router = express.Router();
 
-//get pins
-router.get("/pins", async (req, res) => {
-  try {
-    const pins = await Pin.find().exec();
-    res.json(pins);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ 
-		status: "FAILED",
-		message: "Error fetching pins" });
-  }
-});
+// Get all pins
+router.get("/", PinController.getAllPins);
 
-//add pin
-router.post("/add-pin", async (req, res) => {
-  try {
-    const { description, color, addedBy, location } = req.body;
-    if (!description || !color || !addedBy || !location) {
-      return res.status(400).json({
-        status: "FAILED",
-        message: "Missing required fields",
-      });
-    }
-    if (!color.match(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/)) {
-      return res.status(400).json({
-        status: "FAILED",
-        message: "Invalid color format",
-      });
-    }
-    const pin = new Pin({
-      description,
-      color,
-      addedBy,
-      location: {
-        type: "Point",
-        coordinates: location.split(",").map(Number),
-      },
-    });
-	//add pin
-    await pin.save();
-    res.json({
-      status: "SUCCESS",
-      message: "Pin added successfully",
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      status: "",
-      message: "Error adding pin",
-    });
-  }
-});
+// Get a single pin
+router.get("/:id", PinController.getPin);
+
+// Add pin
+router.post("/", PinController.createPin);
+
+// Edit pin
+router.put("/:id", PinController.editPin);
+
+// Delete pin
+router.delete("/:id", PinController.deletePin);
 
 export default router;
